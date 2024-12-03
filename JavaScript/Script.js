a22ae59c59557626d8b2ed0c37e70cd581225095
Lavioo <volcanic.lavioo@gmail.com>
2024-12-03 16:28:24 -0500

    Update

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,38 +1,70 @@
+window.onload=cycleImage()
+
+
 function news() {
-    alert("Nouveaute")
+    alert("Nouveaute");
 }
 
 function models(){
-    alert("models")
+    alert("models");
 }
 
 function home(){
     if(window.location.pathname == "/Home.html"){
-        window.location.reload()
+        window.location.reload();
     }else{
-        window.location.href("/Home.html")
+        window.location.href("/Home.html");
     }
 }
 
 function about(){
-    alert("about")
+    alert("about");
 }
   
 function rechercher(){
-    alert("Bonjour")
+    alert("Bonjour");
 }
 
 function cycleImage(){
-    image_auto = new Set ["Images\auto1 1.jpg", "Images\auto2.jpg", "Images\auto3.png", "Images\auto4.jpg"]
-    let img  = []
-    for (let i = 0; i < 3; i ++){
-        img[i] = document.createElement("img" + i)
-        img[i].src = image_auto [i]
-    }
-    while(true){
-        for (let i = 0; i <image_auto.length; i++){
-        document.body.appendChild(img[i]);
-        setTimeout(3000)
+    var cycle = document.getElementById('cycle');
+    const lienCycle = ["/auto1 1.jpg", "/auto2.jpg", "/auto3.png", "/auto4.jpg"];
+    var dir = "/ImagesCycle";
+    if (!document.images)
+        return;
+    var num = 0
+    var changeImg = function(){
+        var length = lienCycle.length;
+        cycle.src = dir + lienCycle[num++]
+        if (num == length){
+            num = 0;
         }
+    };
+    setInterval(changeImg(), 3000)
+}
+
+index = 0;
+
+function setIndex(num){
+    index = num;
+}
+
+function startSlideshow(containerId, interval) {
+    const container = document.getElementById(containerId);
+    const images = container.getElementsByTagName('img');
+    
+    for(i = 1; i <images.length; i++){
+        images[i].style.display = 'none';
     }
+    
+    if (images.length === 0) return; // Aucune image
+
+    // Affiche la première image
+    images[index].style.display = 'block';
+
+    // Changer d'image périodiquement
+    setInterval(() => {
+        images[index].style.display = 'none'; // Cacher l'image actuelle
+        index = (index + 1) % images.length; // Passer à l'image suivante
+        images[index].style.display = 'block'; // Afficher la nouvelle image
+    }, interval);
 }
