8ea81df35bdc12a4072d7dfd5f51ca1dcd21155e
Lavioo <volcanic.lavioo@gmail.com>
2024-12-07 18:07:46 -0500

    New commit

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -22,4 +22,17 @@ function rechercher(){
     alert("Bonjour");
 }
 
+function slideShow(){
+    const photos = document.getElementsByClassName("cycle");
+    const buttons = document.getElementsByClassName("slideButton");
+    let i = 0;
+    var displayedImg = photos[i];
+    var buttonsActive = i;
+    for(j = 0; j < buttons.length; j++){
+        if (buttons[j] != button[displayedImg]){
+            buttons[j].style.color = "#2D2D2A"
+        }
+    }
+    
 
+}
