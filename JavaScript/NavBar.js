50ab57ac1044898b52d6edf1b7aa96809dc06db1
Lavioo <volcanic.lavioo@gmail.com>
2024-12-12 21:43:22 -0500

    final modifs

---
+++ JavaScript/NavBar.js
@@ -0,0 +1,49 @@
+function news() {
+    if(window.location.pathname == "/HTML/Nouveautes.html"){
+        window.location.reload();
+    }else{
+        window.location = '/HTML/Nouveautes.html';
+    }
+}
+
+function home(){
+    if(window.location.pathname == "/Home.html"){
+        window.location.reload();
+    }else{
+        window.location = '/Home.html';
+    }
+}
+
+function about(){
+    if(window.location.pathname == "/HTML/A_propos.html"){
+        window.location.reload();
+    }else{
+        window.location = '/HTML/A_propos.html';
+    }
+}
+  
+function rechercher(){
+    alert("Cette fonctionnalité n'a pas pu être configuré");
+}
+
+function bmw(){
+    if(window.location.pathname == "/HTML/Modele/BMW.html"){
+        window.location.reload();
+    }else{
+        window.location = '/HTML/Modele/BMW.html';
+    }
+}
+function audi(){
+    if(window.location.pathname == "/HTML/Modele/Audi.html"){
+        window.location.reload();
+    }else{
+        window.location = '/HTML/Modele/Audi.html';
+    }
+}
+function mercedes(){
+    if(window.location.pathname == "/HTML/Modele/mercedes.html"){
+        window.location.reload();
+    }else{
+        window.location = '/HTML/Modele/mercedes.html';
+    }
+}
---
+++ JavaScript/SlideShow.js
@@ -0,0 +1,26 @@
+let slideIndex = 0;
+showSlides();
+
+function currentSlide(n){
+    showSlides(slideIndex = n)
+}
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("Slides");
+  let dots = document.getElementsByClassName("dots")
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++){
+    dots[i].className = dots[i].className.replace(" active", "")
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 6000); // Change image every 6 seconds
+  
+}
+
+//Source: https://www.w3schools.com/howto/howto_js_slideshow.asp
